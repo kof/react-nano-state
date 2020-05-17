28031f5fa017580cd7d78dc7fe8233622cd7c9b7
Oleg Isonen <oleg008@gmail.com>
2020-05-17 17:21:29 +0200

    dispatchers need to be inside a container

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import * as React from "react";
 
-const dispatchersMap = new Map();
-
 export const createValueContainer = (initialValue) => {
-  const container = { value: initialValue, dispatchersMap };
+  const container = { value: initialValue, dispatchers: new Map() };
   container.dispatch = (value) => {
     if (value === container.value) return;
     container.value = value;
-    const dispatchers = dispatchersMap.values();
+    const dispatchers = container.dispatchers.values();
     for (const dispatch of dispatchers) {
       dispatch(value);
     }
@@ -19,9 +17,9 @@ export const useValue = (container) => {
   const ref = React.useRef();
   const [value, dispatch] = React.useState(container.value);
   // When component unmounts, we want to forget it's dispatcher reference.
-  React.useEffect(() => () => container.dispatchersMap.delete(ref), []);
+  React.useEffect(() => () => container.dispatchers.delete(ref), []);
   // We always safe the latest known dispatch reference based on component instance.
-  container.dispatchersMap.set(ref, dispatch);
+  container.dispatchers.set(ref, dispatch);
 
   return [value, container.dispatch];
 };
